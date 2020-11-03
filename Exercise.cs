fd28f7101460947bcdafc7ffbd9c1dc20ba600ad
Luke Fisher <thebigfishluke@gmail.com>
2020-11-03 16:11:52 -0500

    empty exercise

---
+++ Exercise.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace luke_code
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
